0db0f80188a08592f678e44dabea700c1a6a32bd
leo153624nid <barjastroy@gmail.com>
2022-04-22 19:52:38 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,8 +5,18 @@ const contents = document.querySelectorAll('.program-line__content');
 contents.forEach( (elem) => {
     const title = elem.querySelector('.program-line__title');
     const descr = elem.querySelector('.program-line__descr');
+    const descrAll = elem.querySelectorAll('.program-line__descr');
 
     title.addEventListener('click', () => {
-        descr.classList.toggle('active');
+        noActive();
+        descr.classList.add('active');
     })
-} )
+} );
+
+function noActive () {
+    const descrAll = document.querySelectorAll('.program-line__descr');
+    
+    descrAll.forEach ( (elem) => {
+        elem.classList.remove('active');
+    } )
+}
