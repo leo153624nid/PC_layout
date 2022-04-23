321d6811b5f8ef9125dabf125191fde765b79935
leo153624nid <barjastroy@gmail.com>
2022-04-23 10:28:41 +0300

    Create accordeon.js

---
+++ js/accordeon.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const contents = document.querySelectorAll('.program-line__content');
+const descrAll = document.querySelectorAll('.program-line__descr');
+
+contents.forEach( (elem) => {
+    const title = elem.querySelector('.program-line__title');
+    const descr = elem.querySelector('.program-line__descr');
+
+    title.addEventListener('click', () => {
+        noActive(descrAll);
+        descr.classList.add('active');
+    })
+} );
+
+// Функция получает массив обьектов с нужным классом и удаляет у всех класс "active"
+function noActive (arr) {    
+    arr.forEach ( (elem) => {
+        elem.classList.remove('active');
+    } )
+}
