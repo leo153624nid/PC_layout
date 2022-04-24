1cde267f8a3d15a9358c41b432be54070f5138f0
leo153624nid <barjastroy@gmail.com>
2022-04-24 07:56:44 +0300

    Update accordeon.js

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,21 +1,24 @@
 'use strict'
+;(function () {
+    const contents = document.querySelectorAll('.program-line__content');
+    const descrAll = document.querySelectorAll('.program-line__descr');
 
-const contents = document.querySelectorAll('.program-line__content');
-const descrAll = document.querySelectorAll('.program-line__descr');
+    contents.forEach( (elem) => {
+        const title = elem.querySelector('.program-line__title');
+        const descr = elem.querySelector('.program-line__descr');
 
-contents.forEach( (elem) => {
-    const title = elem.querySelector('.program-line__title');
-    const descr = elem.querySelector('.program-line__descr');
+        title.addEventListener('click', () => {
+            noActive(descrAll);
+            descr.classList.add('active');
+        })
+    } );
 
-    title.addEventListener('click', () => {
-        noActive(descrAll);
-        descr.classList.add('active');
-    })
-} );
+    // Функция получает массив обьектов с нужным классом и удаляет у всех класс "active"
+    function noActive (arr) {    
+        arr.forEach ( (elem) => {
+            elem.classList.remove('active');
+        } )
+    }
+})();
 
-// Функция получает массив обьектов с нужным классом и удаляет у всех класс "active"
-function noActive (arr) {    
-    arr.forEach ( (elem) => {
-        elem.classList.remove('active');
-    } )
-}
+
