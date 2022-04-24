864f172971c986d6cdba07dd2d3208205ce0bb99
leo153624nid <barjastroy@gmail.com>
2022-04-24 07:56:46 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,47 +1,49 @@
 'use strict'
 
-const modalBtn = document.querySelector('.modal__button');
-const modal = document.querySelector('.modal');
-const courseBtn = document.querySelector('.course__button');
+;(function(){
+    const modalBtn = document.querySelector('.modal__button');
+    const modal = document.querySelector('.modal');
+    const courseBtn = document.querySelector('.course__button');
 
-// Появление модального окна по кнопке "Оставить заявку"
-modalBtn.addEventListener('click', () => {
-    modal.style.display = 'flex';
-})
+    // Появление модального окна по кнопке "Оставить заявку"
+    modalBtn.addEventListener('click', () => {
+        modal.style.display = 'flex';
+    })
 
-// Появление модального окна по кнопке "Заказать курс"
-courseBtn.addEventListener('click', () => {
-    modal.style.display = 'flex';
-})
+    // Появление модального окна по кнопке "Заказать курс"
+    courseBtn.addEventListener('click', () => {
+        modal.style.display = 'flex';
+    })
 
-// Закрытие модального окна по клику вне зоны модального окна
-modal.addEventListener('click', (event) => {
-    const modalContent = event.target.closest('.modal__inner');
-    
-    if (!modalContent) {
+    // Закрытие модального окна по клику вне зоны модального окна
+    modal.addEventListener('click', (event) => {
+        const modalContent = event.target.closest('.modal__inner');
+        
+        if (!modalContent) {
+            modal.style.display = '';
+        } 
+    })
+
+    // Создание "Х" для закрытия модального окна
+    let xBtn = document.createElement('div');
+    // xBtn.className = 'xclose';  не используется
+    xBtn.innerText = 'X';
+
+    modalInner.prepend (xBtn);
+
+    xBtn.style.cssText = `
+        font-size:20px; 
+        position:relative; 
+        display:inline; 
+        padding:10px; 
+        bottom:100px; 
+        left:330px; 
+        cursor:pointer;
+    `; 
+
+    // Закрытие модального окна по клику "Х"
+    xBtn.addEventListener('click', () => {
         modal.style.display = '';
-    } 
-})
-
-// Создание "Х" для закрытия модального окна
-let xBtn = document.createElement('div');
-// xBtn.className = 'xclose';  не используется
-xBtn.innerText = 'X';
-
-modalInner.prepend (xBtn);
-
-xBtn.style.cssText = `
-    font-size:20px; 
-    position:relative; 
-    display:inline; 
-    padding:10px; 
-    bottom:100px; 
-    left:330px; 
-    cursor:pointer;
-`; 
-
-// Закрытие модального окна по клику "Х"
-xBtn.addEventListener('click', () => {
-    modal.style.display = '';
-}) 
-
+    }) 
+})();
+    
