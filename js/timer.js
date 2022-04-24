632b8fa7956cc0ae838534fc8aa1430bba34750c
leo153624nid <barjastroy@gmail.com>
2022-04-24 08:06:55 +0300

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -6,5 +6,9 @@
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    
+
+    const hoursBlock = document.querySelector('.timer__hours');
+    const minutesBlock = document.querySelector('.timer__minutes');
+    const secondsBlock = document.querySelector('.timer__seconds');
+
 })();
